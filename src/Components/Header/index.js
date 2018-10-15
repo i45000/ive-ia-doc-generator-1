14eb0e1b846c9f1d927d085238e3d154af02214a
Mark Lai <marklai1998@gmail.com>
2018-10-16 01:29:12 +0800

    feat: add box component

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -4,14 +4,12 @@ import ghLogo from "../../assets/github-logo.svg";
 import logo from "../../assets/logo.svg";
 import classes from "./index.css";
 
-export const Header = () => {
-  return (
-    <header className={classes.header}>
-      <img src={logo} className={classes.logo} alt="logo" />
-      <div className={classes.title}>IVE IA Document Generator</div>
-      <a href="https://github.com/marklai1998/IVE-IA-Doc-Generator">
-        <img src={ghLogo} className={classes.ghLogo} alt="git-hub-logo" />
-      </a>
-    </header>
-  );
-};
+export const Header = () => (
+  <header className={classes.header}>
+    <img src={logo} className={classes.logo} alt="logo" />
+    <div className={classes.title}>IVE IA Document Generator</div>
+    <a href="https://github.com/marklai1998/IVE-IA-Doc-Generator">
+      <img src={ghLogo} className={classes.ghLogo} alt="git-hub-logo" />
+    </a>
+  </header>
+);
