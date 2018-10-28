3f36f06052a872d70d1603f81418468d25bc103c
Mark Lai <marklai1998@gmail.com>
2018-10-29 03:19:53 +0800

    feat: added invalid btn for import method

--- src/Containers/ImportExport/Import/index.js
+++ src/Containers/ImportExport/Import/index.js
@@ -1,26 +1,65 @@
 // @flow strict
 
+import classNames from 'classnames'
 import React from 'react'
 
+import { inputMap } from '../../../inputMap'
+import { validateImport } from '../../../Utils/formHelper'
 import classes from './index.css'
 
-export class Import extends React.PureComponent<{}> {
-  getDataFromClipboard = () => {
-    navigator.clipboard.readText().then(text => {
-      console.log('Pasted content: ', text)
-    })
+type State = {
+  invalid: {
+    clipboard: boolean
+  }
+}
+
+export class Import extends React.PureComponent<{}, State> {
+  state = {
+    invalid: {
+      clipboard: false
+    }
+  }
+
+  getDataFromClipboard = async () => {
+    try {
+      const clipboardText = await navigator.clipboard.readText()
+      const textToObject = await JSON.parse(clipboardText)
+      const isValid = validateImport(textToObject, inputMap)
+      if (!isValid) throw new Error('invalid fields')
+      console.log('success!')
+    } catch (error) {
+      this.setState({ invalid: { clipboard: true } })
+      setTimeout(() => {
+        this.setState({ invalid: { clipboard: false } })
+      }, 3000)
+    }
   }
 
   render () {
+    const { invalid } = this.state
     return (
       <React.Fragment>
         <div className={classes.container}>
+          <span className={classes.or}>Or</span>
           <button
             type='button'
             onClick={this.getDataFromClipboard}
-            className={classes.clipboardBtn}
+            className={classNames(classes.clipboardBtn, {
+              [classes.invalid]: invalid.clipboard
+            })}
+            disabled={invalid.clipboard}
           >
-            Get From Clipboard
+            {invalid.clipboard ? (
+              <React.Fragment>
+                <i className='fas fa-times' />
+                Invalid Clipboard Text
+              </React.Fragment>
+            ) : (
+              <React.Fragment>
+                <i className='fas fa-clipboard' />
+                Get From Clipboard
+              </React.Fragment>
+            )}
           </button>
         </div>
       </React.Fragment>
--- src/Utils/formHelper.js
+++ src/Utils/formHelper.js
@@ -26,9 +26,9 @@ export const getCompletePercentage = (
   inputMap: typeof mapReference,
   formState: { [string]: { [string]: any } }
 ) =>
-  Object.keys(formState).reduce((accObj, categoryKey) => {
+  R.keys(formState).reduce((accObj, categoryKey) => {
     const stateCategoryObj = R.prop(categoryKey, formState)
-    const completeCount = Object.keys(stateCategoryObj).reduce(
+    const completeCount = R.keys(stateCategoryObj).reduce(
       (accCount, fieldKey) => {
         const field = R.prop(fieldKey, stateCategoryObj)
         const required = R.path([categoryKey, fieldKey, 'required'], inputMap)
@@ -38,7 +38,21 @@ export const getCompletePercentage = (
       },
       0
     )
-    const percentage =
-      (completeCount / Object.keys(stateCategoryObj).length) * 100
+    const percentage = (completeCount / R.keys(stateCategoryObj).length) * 100
     return { ...accObj, [categoryKey]: percentage }
   }, {})
+
+export const validateImport = (
+  inputObject: Object,
+  inputMap: typeof mapReference
+) => {
+  const hasError = R.keys(inputMap).some(categoryKey => {
+    if (!R.has(categoryKey, inputObject)) return true
+    const categoryObj = R.prop(categoryKey, inputMap)
+    return R.keys(categoryObj).some(inputKey => {
+      if (!R.has(inputKey, inputObject[categoryKey])) return true
+      return false
+    })
+  })
+  return hasError === false
+}
