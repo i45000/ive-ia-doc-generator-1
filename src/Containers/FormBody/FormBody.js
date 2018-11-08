2043707f8119b4691ba7bbd294eeb54a02fc8c25
Mark Lai <marklai1998@gmail.com>
2018-11-09 02:16:16 +0800

    fix: fail to set complete percentage after preserved redux state

---
+++ src/Containers/FormBody/FormBody.js
@@ -0,0 +1,85 @@
+// @flow strict
+
+import React from 'react'
+import { connect } from 'react-redux'
+import { Form } from 'react-redux-form/immutable'
+
+import { IADetail } from '../../Components/IADetail'
+import { IASupervisor } from '../../Components/IASupervisor'
+import { PersonalInfo } from '../../Components/PersonalInfo'
+import { ProgramInfo } from '../../Components/ProgramInfo'
+import { SiteVisit } from '../../Components/SiteVisit'
+import { inputMap } from '../../inputMap'
+import { formSelector } from '../../redux/form/selectors'
+import {
+  generateFile,
+  getCompletePercentage,
+  getInitPercentage
+} from '../../utils/formHelper'
+
+type State = {
+  percentage: { [string]: number }
+}
+
+type Props = {
+  form: Object
+}
+
+class FormBody extends React.PureComponent<Props, State> {
+  state = {
+    percentage: getInitPercentage(inputMap)
+  }
+
+  componentDidMount () {
+    const percentageObj = getCompletePercentage(
+      inputMap,
+      this.props.form.toJS()
+    )
+    this.setState({ percentage: percentageObj })
+  }
+
+  handleChange = (values: Object) => {
+    const percentageObj = getCompletePercentage(inputMap, values.toJS())
+    this.setState({ percentage: percentageObj })
+  }
+
+  handleSubmit = (values: Object) => {
+    generateFile(values.toJS())
+  }
+
+  render () {
+    const { percentage } = this.state
+    return (
+      <Form
+        model='form'
+        onSubmit={val => this.handleSubmit(val)}
+        onChange={this.handleChange}
+        id='form'
+      >
+        <ProgramInfo percentage={percentage.programInfo} />
+        <PersonalInfo
+          percentage={
+            (percentage.personalInfo + percentage.emergencyContact) / 2
+          }
+        />
+        <IASupervisor percentage={percentage.iaSupervisor} />
+        <IADetail
+          percentage={
+            (percentage.iaCompany +
+              percentage.iaMentor +
+              percentage.iaJob +
+              percentage.iaRequirement) /
+            4
+          }
+        />
+        <SiteVisit percentage={percentage.siteVisit} />
+      </Form>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  form: formSelector(state)
+})
+
+export default connect(mapStateToProps)(FormBody)
